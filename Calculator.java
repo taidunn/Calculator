bef874ce7ded167c425dea22c546d5010d3cdd66
Tai <macuser@macs-mbp-6.attlocal.net>
2018-12-17 01:24:11 -0500

    Update Calculator.java
    First Patch Through

---
+++ Calculator.java
@@ -0,0 +1,27 @@
+//IN PROGRESS
+/*INCOMPLETE*/
+import java.util.Scanner;
+
+public class HelloWorld1
+{
+	public static int NumberOne = 0;
+	public static int NumberTwo = 0;
+	public static void FirstNumber()
+	{
+		System.out.println("First Number");
+	}
+	public static void SecondNumber()
+	{
+		System.out.println("Second Number");
+	}
+	public static void MASD()
+	{
+		System.out.println("Multiply, Add, Subtract, Divide");
+	}
+	public static void main(String[] args)
+	{
+		FirstNumber();
+		SecondNumber();
+		MASD();
+	}
+}
