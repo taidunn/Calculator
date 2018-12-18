abc2740a0f7b68d048358b0d3a68d1a4928657f2
Tai <macuser@macs-mbp-6.attlocal.net>
2018-12-17 22:43:31 -0500

    Update Calculator.java
    First Entry

--- Calculator.java
+++ Calculator.java
@@ -1,22 +1,54 @@
-//IN PROGRESS
+//Version 1.0
 /*INCOMPLETE*/
+
 import java.util.Scanner;
 
-public class HelloWorld1
+public class Calculator
 {
+	public static long result = 0;
 	public static int NumberOne = 0;
 	public static int NumberTwo = 0;
+	public static String Choose = "";
 	public static void FirstNumber()
 	{
-		System.out.println("First Number");
+		Scanner OneEnt = new Scanner(System.in);
+		System.out.print("Please enter your first number: ");
+		NumberOne = OneEnt.nextInt();
 	}
 	public static void SecondNumber()
 	{
+		Scanner TwoEnt = new Scanner(System.in);
+		System.out.print("Please enter your  number: ");
+		NumberTwo = TwoEnt.nextInt();
 		System.out.println("Second Number");
 	}
 	public static void MASD()
 	{
-		System.out.println("Multiply, Add, Subtract, Divide");
+		Scanner MASD = new Scanner(System.in);
+		System.out.print("Declare if you want to (M)ultiply, (S)ubtract, (A)dd, or (D)ivide: ");
+		//System.out.print("M, A, S, or D?: ");
+		String temp = "";
+		temp = MASD.nextLine();
+		Choose = temp.toUpperCase();
+		switch (Choose)
+		{
+		case "M":
+			result = NumberOne * NumberTwo;
+			break;
+
+		case "A":
+			result = NumberOne + NumberTwo;
+			break;
+
+		case "S":
+			result = NumberOne - NumberTwo;
+			break;
+
+		case "D":
+			result = NumberOne / NumberTwo;
+			break;
+		}
+		System.out.println(result);
 	}
 	public static void main(String[] args)
 	{
