840d8b1956e7504f9f588acbf4f99c9c0c857817
Tai <macuser@macs-mbp-6.attlocal.net>
2018-12-18 21:09:24 -0500

    Update Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,59 @@
+//Version 1.0
+/*INCOMPLETE*/
+
+import java.util.Scanner;
+
+public class Calculator
+{
+	public static long result = 0;
+	public static int NumberOne = 0;
+	public static int NumberTwo = 0;
+	public static String Choose = "";
+	public static void FirstNumber()
+	{
+		Scanner OneEnt = new Scanner(System.in);
+		System.out.print("Please enter your first number: ");
+		NumberOne = OneEnt.nextInt();
+	}
+	public static void SecondNumber()
+	{
+		Scanner TwoEnt = new Scanner(System.in);
+		System.out.print("Please enter your  number: ");
+		NumberTwo = TwoEnt.nextInt();
+		System.out.println("Second Number");
+	}
+	public static void MASD()
+	{
+		Scanner MASD = new Scanner(System.in);
+		System.out.print("Declare if you want to (M)ultiply, (S)ubtract, (A)dd, or (D)ivide: ");
+		//System.out.print("M, A, S, or D?: ");
+		String chosen = "";
+		chosen = MASD.nextLine();
+		Choose = chosen.toUpperCase();
+		switch (Choose)
+		{
+		case "M":
+			result = NumberOne * NumberTwo;
+			break;
+
+		case "A":
+			result = NumberOne + NumberTwo;
+			break;
+
+		case "S":
+			result = NumberOne - NumberTwo;
+			break;
+
+		case "D":
+			result = NumberOne / NumberTwo;
+			break;
+		}
+		System.out.println(result);
+	}
+	public static void main(String[] args)
+	{
+		FirstNumber();
+		SecondNumber();
+		MASD();
+	}
+}
